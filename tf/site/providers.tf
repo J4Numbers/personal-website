3b8960c34340058252e53acb45bd9c389f8cbc2d
Jayne Doe <j4numbers@gmail.com>
2022-04-05 01:44:29 +0100

    build(tf): allow patch version updates
    Signed-off-by: Jayne Doe <j4numbers@gmail.com>

--- tf/site/providers.tf
+++ tf/site/providers.tf
@@ -11,7 +11,7 @@ provider "aws" {
 }
 
 terraform {
-  required_version = "1.1.0"
+  required_version = "~> 1.1.0"
 
   backend "s3" {
     key     = "personal-website.tfstate"
@@ -22,7 +22,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "4.0.0"
+      version = "~> 4.8.0"
     }
   }
 }
